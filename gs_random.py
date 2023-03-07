6552743cbc614230ee87bf215cad2623246b1141
Georgy Makarov <makarov.prdx@gmail.com>
2023-03-07 19:13:20 +0300

    optimized for work

--- gs_random.py
+++ gs_random.py
@@ -2,21 +2,22 @@ from random import randint
 from threading import Thread
 
 
+QUANTITY = 16
+
 class LocusRandomizer(Thread):
     def __init__(self):
         Thread.__init__(self)
         self.pos = []
-        for i in range(8):
+        for i in range(QUANTITY):
             x = randint(-550, 550)
             y = randint(-550, 550)
             z = randint(0, 400)
-            yaw = randint(0, int(2*3.14))
-            self.pos.append((x, y, z, yaw))
+            self.pos.append((x, y, z))
         self.main()
 
     def main(self):
-        for i in range(8):
-            x, y, z, yaw = self.pos[i][0], self.pos[i][1], self.pos[i][2], self.pos[i][3]
+        for i in range(QUANTITY):
+            x, y, z = self.pos[i][0], self.pos[i][1], self.pos[i][2]
             x += randint(-5, 5)
             y += randint(-5, 5)
             while True:
@@ -25,8 +26,7 @@ class LocusRandomizer(Thread):
                     break
                 else:
                     z = 0
-            yaw += randint(-1, 1)
-            self.pos[i] = (x, y, z, yaw)
+            self.pos[i] = (x, y, z)
 
     def get_pos(self):
         if len(self.pos) > 3:
--- main.py
+++ main.py
@@ -1,42 +1,52 @@
 import sys
-import time
-from math import pi, sin, cos
 from direct.showbase.ShowBase import ShowBase
-from direct.task import Task
 from direct.actor.Actor import Actor
-from direct.interval.IntervalGlobal import Sequence
 from panda3d.core import *
 from gs_random import LocusRandomizer
 
+QUANTITY = 16
+# TODO: окошко с координатами каждого дрона, дрон - сфера изменяемого цвета d/r 10 см, функция записи шоу,
+# TODO: привязка функций записи и запуска логгера к клавишам и вывести табличку с управлением в окно
+# 11x11x4, drone diameter = 0.1
 
-def drawStartPoint(render):
+
+def draw_axis(render):
     x_axis = LineSegs("lines")
     x_axis.moveTo(0, 0, 0)
-    x_axis.drawTo(2, 0, 0)
-    x_axis.setThickness(2)
+    x_axis.drawTo(1, 0, 0)
+    x_axis.setThickness(1)
     x_node = NodePath(x_axis.create())
     x_node.reparentTo(render)
-    x_node.setColor((2.0, 0.0, 0.0, 0.5), 1)
+    x_node.setColor((1.0, 0.0, 0.0, 1), 1)
 
     y_axis = LineSegs("lines")
     y_axis.moveTo(0, 0, 0)
-    y_axis.drawTo(0, 2, 0)
-    y_axis.setThickness(2)
+    y_axis.drawTo(0, 1, 0)
+    y_axis.setThickness(1)
     y_node = NodePath(y_axis.create())
     y_node.reparentTo(render)
-    y_node.setColor((0.0, 2.0, 0.0, 0.5), 1)
+    y_node.setColor((0.0, 1.0, 0.0, 1), 1)
 
     z_axis = LineSegs("lines")
     z_axis.moveTo(0, 0, 0)
-    z_axis.drawTo(0, 0, 2)
-    z_axis.setThickness(2)
+    z_axis.drawTo(0, 0, 1)
+    z_axis.setThickness(1)
     z_node = NodePath(z_axis.create())
     z_node.reparentTo(render)
-    z_node.setColor((0.0, 0.0, 2.0, 0.5), 1)
+    z_node.setColor((0.0, 0.0, 1.0, 1), 1)
 
 
-def drawGrid(render):
+def draw_grid(render):
     grid = LineSegs("lines")
+    grid.moveTo(-5.5, -5.5, 0)
+    for i in range(11):
+        grid.moveTo(-5.5+i, -5.5, 0)
+        grid.drawTo(-5.5+i, 5.5, 0)
+    grid.moveTo(-5.5, -5.5, 0)
+    for i in range(11):
+        grid.moveTo(-5.5, -5.5+i, 0)
+        grid.drawTo(5.5, -5.5+i, 0)
+
     grid.moveTo(-5.5, -5.5, 0)
     grid.drawTo(5.5, -5.5, 0)
     grid.drawTo(5.5, 5.5, 0)
@@ -56,52 +66,38 @@ def drawGrid(render):
     grid.setThickness(1)
     grid_node = NodePath(grid.create())
     grid_node.reparentTo(render)
+    grid_node.setColor((0.2, 0.2, 0.2, 1), 1)
+
 
 class Locus3D(ShowBase):
-
     def __init__(self):
         ShowBase.__init__(self)
         self.gs = LocusRandomizer()
-        drawStartPoint(self.render)
-        drawGrid(self.render)
+
+        draw_axis(self.render)
+        draw_grid(self.render)
         base.setBackgroundColor(0, 0, 0)
 
-        # self.scene = self.loader.loadModel("models/environment")
-        #
-        # # Reparent the model to render.
-        #
-        # self.scene.reparentTo(self.render)
-        #
-        # # Apply scale and position transforms on the model.
-        #
-        # self.scene.setScale(0.25, 0.25, 0.25)
-        #
-        # self.scene.setPos(-8, 42, 0)
-
         self.drones = []
-        for i in range(8):
+        for i in range(QUANTITY):
             drone = Actor("models/jack")
             drone.setScale(0.1, 0.1, 0.1)
             drone.reparentTo(self.render)
             self.drones.append(drone)
-        print(self.drones[1])
-        # Load and transform the panda actor
 
         self.accept("escape", sys.exit)
-
         taskMgr.add(self.move, 'moveTask')
 
     def move(self, task):
         self.gs.main()
+        
         pos = self.gs.get_pos()
         print(pos)
-        drones = []
-        if pos is not None: #11x11x4, drone diameter = 0.1
-            for i in range(len(pos)):
+        if pos is not None:
+            for i in range(QUANTITY):
                 self.drones[i].setX(pos[i][0]/100)
                 self.drones[i].setY(pos[i][1]/100)
                 self.drones[i].setZ(pos[i][2]/100)
-                self.drones[i].setH(pos[i][3])
         return task.cont
 
 
