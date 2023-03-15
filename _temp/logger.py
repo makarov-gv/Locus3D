b30db0f5e4463a080a74735b96c87c64c8e50763
Georgy Makarov <makarov.prdx@gmail.com>
2023-03-15 12:46:12 +0300

    logger fix

--- _temp/logger.py
+++ _temp/logger.py
@@ -1,5 +1,6 @@
 import gs_lps
 import timeit
+import time
 import pandas as pd
 import json
 
@@ -10,11 +11,13 @@ if __name__ == '__main__':
     timer = timeit.default_timer()
     df0 = pd.DataFrame()
     while timeit.default_timer()-timer < 120:
-        data = [gs_lps.get_position(), gs_lps.get_angles(), timeit.default_timer()-timer]
+        data = [list(gs_lps.get_position()), timeit.default_timer()-timer]
         df = pd.DataFrame(
-            [data], columns=['Position', 'Angles', 'Time']
+            [data], columns=['Positions, meters', 'Time passed, seconds']
         )
         df0 = pd.concat([df0, df])
+        print(data)
+        # time.sleep(0.1)
     result = df0.to_json(orient='records')
     parsed = json.loads(result)
     with open('log.json', 'w') as f:
